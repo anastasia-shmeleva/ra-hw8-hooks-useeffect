3d1bc9593503757d940a00d901ced8c94396376c
Anastasia Shmeleva <anastasiashmeleva@Anastasias-MacBook-Air-2.local>
2021-11-20 23:04:30 +0300

    loader move to details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,37 +1,51 @@
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 const Details = (props) => {
-  const { 
-    avatar, 
-    name 
+  const {
+    id,
+    name,
   } = props.children;
 
-  const {
-    city,
-    company,
-    position
-  } = props.children.details;
+  const [isLoading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    if(id) loadUser(id);
+  }, [id]);
+
+  const loadUser = (id) => {
+    setLoading(true)
+    fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${id}.json`)
+    .then((response) => response.json())
+    .then(data => {
+      setUser(data);
+      setLoading(false)
+    })
+    .catch((error) => setError(error));
+  }
 
   return (
     <div className='detais'>
-      <ul className='detais__container'>
+      {error && <p>Something went wrong...</p>}
+      {isLoading && <p className='loading'>Loading...</p>}
+      {user && (
+        <ul className='detais__container'>
         <li className='detais__avatar'>
-          <img src={avatar} alt='profile pic'/></li>
+          <img src={user.avatar} alt='profile pic'/></li>
         <li className=' detais__item detais__name'>{name}</li>
-        <li className='detais__item'>City: {city}</li>
-        <li className='detais__item'>Company: {company}</li>
-        <li className='detais__item'>Position: {position}</li>
+        <li className='detais__item'>City: {user.details.city}</li>
+        <li className='detais__item'>Company: {user.details.company}</li>
+        <li className='detais__item'>Position: {user.details.position}</li>
       </ul>
-    </div>
-  )
+      )}
+  </div>)
 }
 
 Details.propTypes = {
-  avatar: PropTypes.string,
+  id: PropTypes.number,
   name: PropTypes.string,
-  city: PropTypes.string,
-  company: PropTypes.string,
-  position: PropTypes.string,
 }
 
 export default Details;
